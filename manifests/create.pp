9e56b4210488ac6d137f028769b75806ed051f89
Artem Buria <artem_buria@epam.com>
2017-07-10 14:33:41 +0300

    module update

--- manifests/create.pp
+++ manifests/create.pp
@@ -2,17 +2,16 @@ define cr_user::create($user_name, $user_password, $ssh_public, $ssh_private){
   user { $user_name:
       ensure           => present,
       managehome       => true,
-      home             => '/home/${user_name}',
+      home             => "/home/${user_name}",
       password         => $user_password,
       password_max_age => '99999',
       password_min_age => '0',
       shell            => '/bin/bash',
       }
 
-  file { 'ssh_private_key':
-      path => '/home/${user_name}/.ssh/id_rsa'
-      ensure => present,
-      content => '${ssh_private}',
+  file { "/home/${user_name}/.ssh/id_rsa":
+      ensure => file,
+      content => "${ssh_private}",
       require => User[$user_name]
       }
 
@@ -20,7 +19,7 @@ define cr_user::create($user_name, $user_password, $ssh_public, $ssh_private){
       ensure => present,
       user   => $user_name,
       type   => 'ssh-rsa',
-      key    => ssh_public,
+      key    => $ssh_public,
       require => User[$user_name]
       }
 }
