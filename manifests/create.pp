35fbb1f9b7030be0486c7c1513f124966dbb0537
Artem Buria <artem_buria@epam.com>
2017-07-11 12:28:45 +0300

    multiple users with keys creation

--- manifests/create.pp
+++ manifests/create.pp
@@ -7,19 +7,21 @@ define cr_user::create($user_name, $user_password, $ssh_public, $ssh_private){
       password_max_age => '99999',
       password_min_age => '0',
       shell            => '/bin/bash',
+      purge_ssh_keys   => true
       }
 
   file { "/home/${user_name}/.ssh/id_rsa":
-      ensure => file,
-      content => "${ssh_private}",
-      require => User[$user_name]
-      }
+    ensure  => file,
+    content => $ssh_private,
+    require => User[$user_name]
+    }
 
   ssh_authorized_key { $user_name:
-      ensure => present,
-      user   => $user_name,
-      type   => 'ssh-rsa',
-      key    => $ssh_public,
-      require => User[$user_name]
+    ensure  => present,
+    user    => $user_name,
+    type    => 'ssh-rsa',
+    key     => $ssh_public,
+    require => User[$user_name],
+    before  => File["/home/${user_name}/.ssh/id_rsa"]
       }
 }
